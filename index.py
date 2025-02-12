aea73a87e496962bf753006afdcb820c9bc22c58
marchennn <marchendynugraha.2022@student.uny.ac.id>
2025-02-12 09:32:47 +0700

    add config mqtt to postgresql, add post fastapi to mqtt

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
 from config.server import Server
+# from config.mqtt import MQTT
 from config.database import db_connector
 import asyncio
 
 async def main():
+    await db_connector.connect()
     server = Server()
+    # mqtt = MQTT()
 
     server.begin()
+    # mqtt.begin()
     try:
         await server.start()
     finally:
